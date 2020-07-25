821fe1e165373c065e423f726ec9d72edf262213
mohamedgit config --global user.namemohamed-gamal <mohamed gamal>
2020-07-25 20:58:35 +0200

    SearchBar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by mac on 7/25/20.
+//  Copyright © 2020 M.gamal. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult
+{
+    
+    var name = ""
+    var artistName = ""
+    
+}
